7ed7d8c796c16b5402d5685becf584e3a684a302
Riktam Santra <riktamsantra56@gmail.com>
2024-02-16 22:22:48 +0530

    added basic command to generate gemini prompts

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -8,21 +8,23 @@ module.exports = {
      * @param {GuildMember} member 
      */
     async run(client, member) {
-        if(member.guild.id != "758245752836325386") {
+        if (member.guild.id != "758245752836325386") {
             return;
         }
-        
+
         let guild;
-        if(client.guilds.cache) {
+        if (client.guilds.cache) {
             guild = client.guilds.cache.get("758245752836325386");
         }
         else {
             guild = await client.guilds.fetch("758245752836325386");
-        } 
+        }
         let channel = guild.channels.cache.get("1165759162131697674");
 
         let generator = new ServerBannerGenerator();
         await generator.generate(member);
-        await channel.send({files: [{attachment: "test.png"}]});
+        if (channel != undefined) {
+            await channel.send({ files: [{ attachment: "test.png" }] });
+        }
     }
 };
